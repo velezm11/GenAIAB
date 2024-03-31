97b1063c5bb0c450b4b76ddab1abd0fe89438684
Miguel Velez <43326865+velezm11@users.noreply.github.com>
2024-03-31 02:36:02 -0400

    Add files via upload

---
+++ basicmemory.py
@@ -0,0 +1,70 @@
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+import streamlit as st
+import requests
+
+st.set_page_config(page_title="AnyWhere AI Assistant", page_icon="📖")
+st.title("📖 AnyWhere AI Assistant")
+
+"""
+A basic example of using StreamlitChatMessageHistory to help LLMChain remember messages in a conversation.
+The messages are stored in Session State across re-runs automatically. You can view the contents of Session State
+in the expander below. View the
+[source code for this app](https://github.com/langchain-ai/streamlit-agent/blob/main/streamlit_agent/basic_memory.py).
+"""
+
+# Set up memory
+msgs = StreamlitChatMessageHistory(key="langchain_messages")
+if len(msgs.messages) == 0:
+    msgs.add_ai_message("How can I help you?")
+
+view_messages = st.expander("View the message contents in session state")
+
+# Set up the LangChain, passing in Message History
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are an AI chatbot having a conversation with a human."),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{question}"),
+    ]
+)
+
+# Function to send user query to API Gateway and fetch response
+def send_query_to_api(query):
+    api_endpoint = "https://3fj11w32na.execute-api.us-east-1.amazonaws.com/Prod/hello/"
+    payload = {"question": query}
+    response = requests.post(api_endpoint, json=payload)
+    return response.json()
+
+chain_with_history = RunnableWithMessageHistory(
+    prompt,
+    lambda session_id: msgs,
+    input_messages_key="question",
+    history_messages_key="history",
+)
+
+# Render current messages from StreamlitChatMessageHistory
+for msg in msgs.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+# If user inputs a new prompt, generate and draw a new response
+if prompt := st.chat_input():
+    st.chat_message("human").write(prompt)
+    # Note: new messages are saved to history automatically by Langchain during run
+    response = send_query_to_api(prompt)
+    st.chat_message("ai").write(response)
+
+# Draw the messages at the end, so newly generated ones show up immediately
+with view_messages:
+    """
+    Message History initialized with:
+    ```python
+    msgs = StreamlitChatMessageHistory(key="langchain_messages")
+    ```
+
+    Contents of `st.session_state.langchain_messages`:
+    """
+    view_messages.json(st.session_state.langchain_messages)
